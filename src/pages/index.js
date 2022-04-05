acff5f737a51f901ae2dffb01ce08121186c4a3f
unknown <whitebeard086@gmail.com>
2022-04-05 10:33:52 +0100

    create pages component and  implement navbar

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Explore } from "./Explore";
+export { default as ForgotPassword } from "./ForgotPassword";
+export { default as Offers } from "./Offers";
+export { default as Profile } from "./Profile";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
