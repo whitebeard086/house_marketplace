755069b2222d2780ef49424925b837bfddc30a9e
unknown <whitebeard086@gmail.com>
2022-04-06 09:39:10 +0100

    Bug Fix: profile component trying to render the UI before getting data from firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 
-import { Navbar } from "./components";
+import { Navbar, PrivateRoute } from "./components";
 import { Explore, ForgotPassword, Offers, Profile, SignIn, SignUp } from "./pages";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-      <Navbar />
-    </Router>
-    <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+      <ToastContainer />
     </>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Navbar } from "./Navbar";
+export { default as PrivateRoute } from "./PrivateRoute";
+export { default as Spinner } from "./Spinner";
