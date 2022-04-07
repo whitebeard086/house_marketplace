1d71594067d21d51d2372b036fbe9c78af776a7d
unknown <whitebeard086@gmail.com>
2022-04-07 07:53:29 +0100

    fetch listings from firebase and display on the category page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as ListingItem } from "./ListingItem";
 export { default as Navbar } from "./Navbar";
 export { default as OAuth } from "./OAuth";
 export { default as PrivateRoute } from "./PrivateRoute";
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import { db } from "../firebase.config";
