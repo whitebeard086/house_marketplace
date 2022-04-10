6429826f5a63cee76b017ccb70000846efe96ab5
unknown <whitebeard086@gmail.com>
2022-04-10 10:58:47 +0100

    show all listings by the logged in user on their profile page and implement remove listings

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import { db } from "../firebase.config";
+import { ListingItem, Spinner } from "../components";
 import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
 import homeIcon from "../assets/svg/homeIcon.svg";
-
 
 const Profile = () => {
   const auth = getAuth();
 
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -21,6 +32,34 @@ const Profile = () => {
   const { name, email } = formData;
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, "listings");
+
+      const q = query(
+        listingsRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+
+      querySnap.forEach(doc => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   const onLogout = () => {
     auth.signOut();
@@ -32,25 +71,39 @@ const Profile = () => {
       if (auth.currentUser.displayName !== name) {
         // Update display name in firebase
         await updateProfile(auth.currentUser, {
-          displayName: name
-        })
+          displayName: name,
+        });
 
         // Update in firestore
-        const userRef = doc(db, "users", auth.currentUser.uid)
+        const userRef = doc(db, "users", auth.currentUser.uid);
         await updateDoc(userRef, {
-          name
-        })
+          name,
+        });
       }
     } catch (error) {
-      toast.error("Could not update profile details")
+      toast.error("Could not update profile details");
     }
   };
 
-  const onChange = (e) => {
-    setFormData((prevState) => ({
+  const onChange = e => {
+    setFormData(prevState => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
+    }));
+  };
+
+  const onDelete = async listingId => {
+    if (window.confirm("Are you sure you want to delist this property?")) {
+      await deleteDoc(doc(db, "listings", listingId));
+
+      const updatedListings = listings.filter(listing => listing.id === listingId);
+      setListings(updatedListings);
+      toast.success("Property successfully delisted");
+    }
+  };
+
+  if (loading) {
+    return <Spinner />;
   }
 
   return (
@@ -101,6 +154,22 @@ const Profile = () => {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt="arrow right" />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your Listed Properties</p>
+            <ul className="listingsList">
+              {listings.map(listing => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   );
